a2b8588f369df3b786f64c0071a57e13b12ffa73
Jezebel1990 <jezebelguedes@gmail.com>
2024-12-09 16:17:26 -0300

    test: Add heading tests

--- src/components/Heading/test.tsx
+++ src/components/Heading/test.tsx
@@ -1,13 +1,42 @@
-import { render, screen } from '@testing-library/react'
-
+import {  screen } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import 'jest-styled-components'
 import Heading from '.'
+import { renderWithTheme } from 'utils/tests/helpers'
 
 describe('<Heading />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<Heading />)
+  it('should render a white label by default', () => {
+   renderWithTheme(<Heading>Won Games</Heading>);
+   const heading = screen.getByRole('heading', { name: /won games/i });
+   expect(heading).toBeInTheDocument();
+   expect(heading).toHaveStyle({ color: '#FAFAFA' });
+    })
+  })
 
-    expect(screen.getByRole('heading', { name: /Heading/i })).toBeInTheDocument()
+  it('should render a black heading when color is passed', () => {
+    renderWithTheme(<Heading color='black'>Won Games</Heading>);
+    expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyle({
+       color: '#030517'
+    })
+ })
 
-    expect(container.firstChild).toMatchSnapshot()
+ it('should render a heading with a line to the left side', () => {
+  renderWithTheme(<Heading lineLeft>Won Games</Heading>);
+  expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyle({
+     'border-left': '0.7rem solid #9747FF'
   })
 })
+
+it('should render a heading with a line at the bottom', () => {
+  renderWithTheme(<Heading lineBottom>Won Games</Heading>)
+  expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyleRule(
+    'border-bottom',
+    '0.5rem solid #13D7E0',
+    {
+      modifier: '::after'
+    }
+  )
+})
+
+
+  
