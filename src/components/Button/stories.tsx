8958c04f98beabf86ef55977bfee204f28fdee14
Jezebel1990 <jezebelguedes@gmail.com>
2024-12-10 22:56:16 -0300

    feat: Add icon option

--- src/components/Button/stories.tsx
+++ src/components/Button/stories.tsx
@@ -1,17 +1,30 @@
 import { Meta, StoryObj } from '@storybook/react';
+import { AddShoppingCart } from '@styled-icons/material-outlined/AddShoppingCart';
  import Button from '.';
 
  export default {
      title: 'Button',
-      component: Button,
+     component: Button,
       argTypes: {
         children: {
             type: 'string'
+        },
+        icon: {
+            type: 'string'
         }
-      }
+ }  
 } as Meta;
+
  export const Default: StoryObj = (args: any) => <Button {...args} />
 
  Default.args = {
     children: 'Buy now'
+ }
+
+ export const withIcon: StoryObj = (args:any) => <Button {...args} />
+
+ withIcon.args = {
+    size: 'small',
+    children: 'Buy now',
+    icon: <AddShoppingCart />
  }
