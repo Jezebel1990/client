71ba69db3db3c45d504f50eaced66a91a3da0c89
Jezebel1990 <jezebelguedes@gmail.com>
2024-12-18 01:06:14 -0300

    fix: Add Menu desktop changes

--- src/components/Logo/stories.tsx
+++ src/components/Logo/stories.tsx
@@ -3,7 +3,12 @@ import Logo from '.';
 
 export default {
   title: 'Logo',
-  component: Logo
+  component: Logo,
+  parameters: {
+    backgrounds: {
+      default: 'dark'
+    }
+  }
 } as Meta
 
 export function Default(args: any) {
