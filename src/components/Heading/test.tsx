2dde882fdc54877d92adf4e950da3603f88eeddc
Jezebel1990 <jezebelguedes@gmail.com>
2024-12-20 20:47:09 -0300

    fix: Adjusting Heading to contain size and lineColors

--- src/components/Heading/test.tsx
+++ src/components/Heading/test.tsx
@@ -23,7 +23,7 @@ describe('<Heading />', () => {
  it('should render a heading with a line to the left side', () => {
   renderWithTheme(<Heading lineLeft>Won Games</Heading>);
   expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyle({
-     'border-left': '0.7rem solid #9747FF'
+     'border-left': '0.7rem solid #13D7E0'
   })
 })
 
@@ -38,5 +38,35 @@ it('should render a heading with a line at the bottom', () => {
   )
 })
 
+it('should render a Heading with a secondary line color', () => {
+  renderWithTheme(
+    <Heading lineColor="secondary" lineLeft lineBottom>
+      Lorem Ipsum
+    </Heading>
+  )
 
-  
+  const heading = screen.getByRole('heading', { name: /lorem ipsum/i })
+  expect(heading).toHaveStyle({ 'border-left': '0.7rem solid #9747FF'})
+  expect(heading).toHaveStyleRule('border-bottom', '0.5rem solid #9747FF', {
+    modifier: '::after'
+  })
+})
+
+
+
+it('should render a heading with a small size', () => {
+  renderWithTheme(<Heading size="small">Won Games</Heading>)
+  expect(screen.getByRole('heading', { name: /won games/i})).toHaveStyle({
+   'font-size': '1.6rem'
+  })
+
+  expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyleRule(
+    'width',
+    '3rem',
+    {
+      modifier: '::after'
+    }
+  )
+}
+)
+
--- src/types/jest-styled-components.d.ts
+++ src/types/jest-styled-components.d.ts
@@ -1,38 +1,20 @@
-// Types provided from the official repo:
-// https://github.com/styled-components/jest-styled-components/blob/master/typings/index.d.ts
-
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { NewPlugin } from 'pretty-format'
-import { css } from 'styled-components'
-
+import { Plugin, NewPlugin } from 'pretty-format'
 declare global {
   namespace jest {
     interface AsymmetricMatcher {
-      $$typeof: symbol // Alterado para 'symbol' (minúsculo)
-      sample?: string | RegExp | object | Array<any> | ((...args: any[]) => any) // Especificação de tipo de função
+      $$typeof: symbol
+      sample?: string | RegExp | object | Array<any> | Function
     }
-
     type Value = string | number | RegExp | AsymmetricMatcher | undefined
-
     interface Options {
       media?: string
-      modifier?: string | ReturnType<typeof css>
+      modifier?: string
       supports?: string
     }
-
-    interface Matchers<R> {
+    interface Matchers<R, T> {
       toHaveStyleRule(property: string, value?: Value, options?: Options): R
     }
   }
 }
-
-export interface StyledComponentsSerializerOptions {
-  addStyles?: boolean
-  classNameFormatter?: (index: number) => string
-}
-
-export declare const styleSheetSerializer: NewPlugin & {
-  setStyleSheetSerializerOptions: (
-    options?: StyledComponentsSerializerOptions
-  ) => void
-}
+export declare const styleSheetSerializer: Exclude<Plugin, NewPlugin>
