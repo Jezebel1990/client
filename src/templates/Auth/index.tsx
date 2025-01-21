ed89a0cc5a7372254e6f153c6be91c700d5cbef9
Jezebel1990 <jezebelguedes@gmail.com>
2025-01-21 00:58:50 -0300

    style: Add Auth Desktop styles

--- src/templates/Auth/index.tsx
+++ src/templates/Auth/index.tsx
@@ -10,22 +10,27 @@ type AuthProps = {
  const Auth = ({ title, children }: AuthProps) => (
 <S.Wrapper>
  <S.BannerBlock>
+  <S.BannerContent>
   <Logo />
-
+<div>
   <Heading>All your favorite games in one place</Heading>
   <S.Subtitle>
         <strong>WON</strong> is the best and most complete gaming platform.
   </S.Subtitle>
+  </div>
 
   <S.Footer>Won Games 2025 © Todos os Direitos Reservados</S.Footer>
+  </S.BannerContent>
   </S.BannerBlock>
 
   <S.Content>
+    <S.ContentWrapper>
       <Logo color="black" size="large" />
       <Heading color="black" lineColor="secondary" lineLeft>
         {title}
       </Heading>
       {children}
+      </S.ContentWrapper>
     </S.Content>
 </S.Wrapper>
 );
