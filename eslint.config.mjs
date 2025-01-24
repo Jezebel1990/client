2d1ca909eca695227b38e2a9007c2e13c518e039
Jezebel1990 <jezebelguedes@gmail.com>
2025-01-24 20:07:01 -0300

    fix: fix json type files

---
+++ eslint.config.mjs
@@ -0,0 +1,25 @@
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import pluginReact from 'eslint-plugin-react'
+
+export default [
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  { languageOptions: { globals: globals.browser } },
+  {
+    rules: {
+      eqeqeq: 'off',
+      'no-unused-vars': 'error',
+      'prefer-const': ['error', { ignoreReadBeforeAssign: true }],
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      'react/prop-types': 'off',
+      'react/react-in-jsx-scope': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off'
+    }
+  },
+
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended
+]
--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,25 @@
-export default {
+const nextConfig = {
+
   experimental: {
-    cpus: 6, 
+    cpus: 6,
   },
-  staticPageGenerationTimeout: 300, 
+  staticPageGenerationTimeout: 300,
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'i.imgur.com', 
+        protocol: "https",
+        hostname: "i.imgur.com",
       },
     ],
   },
   async headers() {
     return [
       {
-        source: '/(.*)',
+        source: "/(.*)",
         headers: [
           {
-            key: 'X-Content-Type-Options',
-            value: 'nosniff', 
+            key: "X-Content-Type-Options",
+            value: "nosniff",
           },
         ],
       },
@@ -26,3 +27,4 @@ export default {
   },
 };
 
+export default nextConfig;
