a804734451d4cdc4c0c28201b07ac50988bf32d5
Jezebel1990 <jezebelguedes@gmail.com>
2025-02-16 22:59:07 -0300

    feat: Add Grid to Wishlist

--- src/templates/Wishlist/index.tsx
+++ src/templates/Wishlist/index.tsx
@@ -2,8 +2,10 @@ import Base from 'templates/Base'
 
 import Heading from 'components/Heading'
 import Showcase from 'components/Showcase'
-import { Container } from 'components/Container'
 import GameCard, { GameCardProps } from 'components/GameCard'
+
+import { Container } from 'components/Container'
+import { Grid } from 'components/Grid'
 import { HighlightProps } from 'components/Highlight'
 
 import * as S from './styles' 
@@ -26,15 +28,18 @@ import * as S from './styles'
         Wishlist
       </Heading>
 
+
+      <Grid>
       {games?.map((game, index) => (
         <GameCard key={`wishlist-${index}`} {...game} />
       ))}
+    </Grid>
     </Container>
 
     <Showcase
-    title="You may like these games"
-    games={recommendedGames}
-    highlight={recommendedHighlight}
+      title="You may like these games"
+      games={recommendedGames}
+      highlight={recommendedHighlight}
   />
   </Base>
 ) 
