b553d0ba074c2de1c6caf8d864e8cc56e18cabd3
Jezebel1990 <jezebelguedes@gmail.com>
2025-02-19 18:32:33 -0300

    feat: Add PaymentInfo to GameItem

--- src/components/GameItem/stories.tsx
+++ src/components/GameItem/stories.tsx
@@ -17,8 +17,13 @@ export const WithPayment: StoryObj<GameItemProps> = (args:any) => (
   <GameItem {...args} />
 )
 WithPayment.args = {
-  downloadLink: 'https://wongames.com/game/download/21312ndasd'
+  downloadLink: 'https://wongames.com/game/download/21312ndasd',
+  paymentInfo: {
+    flag: 'mastercard',
+    img: '/img/master-card.png',
+    number: '**** **** **** 4326',
+    purchaseDate: 'Purchase made on 07/20/2024 at 20:32'
+  }
 }
-
 
 Default.args = {}
