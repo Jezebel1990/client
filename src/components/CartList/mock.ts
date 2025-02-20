b72511e1b5a87d8ef5f758301c5afa6a8a4dcd1e
Jezebel1990 <jezebelguedes@gmail.com>
2025-02-20 17:49:08 -0300

    feat: Create CartList

---
+++ src/components/CartList/mock.ts
@@ -0,0 +1,12 @@
+export default [
+    {
+      img: 'https://i.imgur.com/AcEXOT2.png',
+      title: 'Red Dead Redemption 2',
+      price: 'R$ 215,00'
+    },
+    {
+      img: 'https://i.imgur.com/zfpB0xb.png',
+      title: 'Borderlands 3',
+      price: 'R$ 215,00'
+    }
+  ]
---
+++ src/components/CartList/stories.tsx
@@ -0,0 +1,33 @@
+import { Meta, StoryObj } from '@storybook/react' 
+import CartList, { CartListProps } from '.'
+
+import mockItems from './mock'
+
+
+ export default {
+     title: 'CartList',
+    component: CartList,
+      args: {
+        items: mockItems,
+        total: 'R$ 330,00'
+      },
+      argTypes: {
+        items: {
+          type: 'string'
+        }
+      },
+      parameters: {
+        backgrounds: {
+          default: 'won-dark'
+        }
+      }
+ } as Meta 
+ 
+ 
+ export const Default: StoryObj<CartListProps> = (args: any) => (
+    <div style={{ maxWidth: 800 }}>
+    <CartList {...args} />
+  </div>
+ ) 
+
+Default.args = {}
