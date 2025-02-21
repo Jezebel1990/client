cc5575cce38b711a6b2a85c6bf7628dfb692197e
Jezebel1990 <jezebelguedes@gmail.com>
2025-02-21 15:37:08 -0300

    style: Add PaymentOptions styles

---
+++ src/components/PaymentOptions/stories.tsx
@@ -0,0 +1,35 @@
+import { Meta, StoryObj } from '@storybook/react'
+
+import PaymentOptions, { PaymentOptionsProps } from '.' 
+import cardsMock from './mock'
+
+
+export default {
+    title: 'PaymentOptions', 
+    component: PaymentOptions,
+    args: {
+        cards: cardsMock
+        },
+  argTypes: {
+    cards: {
+      type: 'string'
+    },
+    handlePayment: {
+      action: 'clicked'
+    }
+  },
+  parameters: {
+    backgrounds: {
+      default: 'won-dark'
+    }
+      }
+} as Meta 
+
+
+export const Default: StoryObj<PaymentOptionsProps> = (args:any) => (
+     <div style={{ padding: 16, maxWidth: 400 }}>
+    <PaymentOptions {...args} />
+    </div>
+)
+
+Default.args = {}
