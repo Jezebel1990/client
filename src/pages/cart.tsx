ee924204bca9f158b2fe8273e08b1baa3a629864
Jezebel1990 <jezebelguedes@gmail.com>
2025-03-09 13:24:33 -0300

    fix: Fix import erros

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -1,13 +1,13 @@
 import Cart, { CartProps } from '@/templates/Cart'
 
-import itemsMock from 'CartList/mock'
+import itemsMock from '@/components/CartList/mock'
 import gamesMock from '@/components/GameCardSlider/mock'
 import higlightMock from '@/components/Highlight/mock'
 import cardsMock from '@/components/PaymentOptions/mock'
 
 export default function CartPage(props: CartProps) {
   return <Cart {...props} />
-}
+}9
 
 export async function getServerSideProps() {
   return {
