dcf1f0a8f7fb34558a95a08724d12e44526edfec
Jezebel1990 <jezebelguedes@gmail.com>
2025-03-10 14:03:54 -0300

    feat: Add CardsList

---
+++ src/components/CardsList/index.tsx
@@ -0,0 +1,20 @@
+import Heading from '@/components/Heading'
+import { PaymentCard } from '@/components/PaymentOptions'
+import * as S from './styles' 
+
+
+const CardsList = ({ cards }: CardsListProps) => (
+<>
+<Heading lineBottom color="black" size="small">
+       My cards
+     </Heading>
+ 
+     {cards?.map((card) => (
+       <S.Card key={card.number}>
+         <img src={card.img} alt={card.flag} />
+         <span>{card.number}</span>
+       </S.Card>
+     ))}
+</>
+)
+export default CardsList
