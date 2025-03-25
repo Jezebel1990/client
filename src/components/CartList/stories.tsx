9232c018c275c13f0915eda3978c246ab9eaf622
Jezebel1990 <jezebelguedes@gmail.com>
2025-03-25 08:23:09 -0300

    fix: Update CartList to have hasButton option

--- src/components/CartList/stories.tsx
+++ src/components/CartList/stories.tsx
@@ -1,33 +1,39 @@
-import { Meta, StoryObj } from '@storybook/react' 
-import CartList, { CartListProps } from '.'
+import { Meta, StoryObj } from '@storybook/react';
+import CartList, { CartListProps } from '.';
 
-import mockItems from './mock'
+import mockItems from './mock';
 
+export default {
+  title: 'CartList',
+  component: CartList,
+  args: {
+    items: mockItems,
+    total: 'R$ 330,00',
+  },
+  argTypes: {
+    items: {
+      control: { type: 'object' }, 
+    },
+  },
+  parameters: {
+    backgrounds: {
+      default: 'won-dark',
+    },
+  },
+} as Meta<CartListProps>;
 
- export default {
-     title: 'CartList',
-    component: CartList,
-      args: {
-        items: mockItems,
-        total: 'R$ 330,00'
-      },
-      argTypes: {
-        items: {
-          type: 'string'
-        }
-      },
-      parameters: {
-        backgrounds: {
-          default: 'won-dark'
-        }
-      }
- } as Meta 
- 
- 
- export const Default: StoryObj<CartListProps> = (args: any) => (
+export const Default: StoryObj<CartListProps> = {
+  render: (args) => (
     <div style={{ maxWidth: 800 }}>
-    <CartList {...args} />
-  </div>
- ) 
+      <CartList {...args} />
+    </div>
+  ),
+};
 
-Default.args = {}
+export const WithButton: StoryObj<CartListProps> = {
+  render: (args) => (
+    <div style={{ maxWidth: 800 }}>
+      <CartList {...args} hasButton />
+    </div>
+  ),
+};
