6a1f1150ee2b8c334325d8d144aca0dd80143d16
Jezebel1990 <jezebelguedes@gmail.com>
2025-04-11 23:59:30 -0300

    fix: Remove apollo example stuff

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,36 +1,17 @@
-import { gql } from '@apollo/client'
-
 import Home, { HomeTemplateProps } from '../templates/Home/index'
 import bannersMock from '../../src/components/BannerSlider/mock'
 import gamesMock from '../../src/components/GameCardSlider/mock'
 import highlightMock from '../../src/components/Highlight/mock'
-import { initializeApollo } from '@/utils/apollo'
-
-const GET_GAMES = gql`
-   query getGames {
-     games {
-       name
-  }
-
- }
- `
 
  export default function Index(props: HomeTemplateProps) {
-   if (props.data) return <p>{JSON.stringify(props.data, null, 2)}</p>
-  
+
   return <Home {...props} />
 }
 
 export async function getServerSideProps() {
 
-const apolloClient = initializeApollo()
- 
-const { data } = await apolloClient.query({ query: GET_GAMES })
-
   return {
     props: {
-      data: data,
-      initialApolloState: apolloClient.cache.extract(),
       banners: bannersMock,
       newGames: gamesMock,
       mostPopularHighlight: highlightMock,
