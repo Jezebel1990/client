ea5637d071472011fa01e2a78e04f6bdf37b6a15
Jezebel1990 <jezebelguedes@gmail.com>
2025-07-15 23:28:13 -0300

    feat: Add GameFragment, HighlightFragment and NewGames

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,24 +13,34 @@ export default function Index(props: HomeTemplateProps) {
 export async function getStaticProps() {
   const apolloClient = initializeApollo()
 
-  const { data } = await apolloClient.query<QueryHome>({ query: QUERY_HOME })
+  const {
+    data: { banners, newGames }
+  } = await apolloClient.query<QueryHome>({ query: QUERY_HOME })
 
   return {
     props: {
       revalidate: 10,
-      banners: data.banners.map((banner) => ({
+      banners: banners.map((banner) => ({
         img: `http://localhost:1337${banner.image?.url}`,
         title: banner.title,
         subtitle: banner.subtitle,
         buttonLabel: banner.button?.label,
         buttonLink: banner.button?.link,
         ...(banner.ribbon && {
-          ribbom: banner.ribbon.text,
+          ribbon: banner.ribbon.text,
           ribbonColor: banner.ribbon.color,
           ribbonSize: banner.ribbon.size
         })
       })),
-      newGames: gamesMock,
+      newGames: newGames.map((game) => ({
+        title: game.name,
+        slug: game.slug,
+        developer: game.developers[0]?.name || 'Unknown',
+        img: game.cover?.url
+          ? `http://localhost:1337${game.cover.url}`
+          : 'https://i.imgur.com/nCI1sci.jpeg',
+        price: game.price
+      })),
       mostPopularHighlight: highlightMock,
       mostPopularGames: gamesMock,
       upcommingGames: gamesMock,
